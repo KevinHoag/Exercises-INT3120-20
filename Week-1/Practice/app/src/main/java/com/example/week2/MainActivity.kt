5fc595e4760f0ade5baff2f554fd3800a2562c4a
X <quangh2019@gmail.com>
2023-09-08 23:27:25 +0700

    add other layout designs

--- Week-1/Practice/app/src/main/java/com/example/week2/MainActivity.kt
+++ Week-1/Practice/app/src/main/java/com/example/week2/MainActivity.kt
@@ -2,10 +2,6 @@ package com.example.week2
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,7 +11,7 @@ import com.example.week2.ui.theme.Week2Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.constraint_layout);
     }
 }
 
