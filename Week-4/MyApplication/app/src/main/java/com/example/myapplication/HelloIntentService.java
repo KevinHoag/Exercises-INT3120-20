14b4732431aa745ce690e5fd6e2db4a3f77608a5
X <quangh2019@gmail.com>
2023-10-04 21:01:42 +0700

    Services

---
+++ Week-4/MyApplication/app/src/main/java/com/example/myapplication/HelloIntentService.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+public class HelloIntentService extends IntentService {
+    private static final int NOTIFICATION_ID = 1;
+
+    public HelloIntentService() {
+        super("HelloIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            Thread.currentThread().interrupt();
+//        }
+        Log.d("asdas", intent.getStringExtra("message"));
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Tiêu đề thông báo")
+                .setContentText("Hello World")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        builder.setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+}
--- Week-4/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Week-4/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.ContentResolver;
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.provider.UserDictionary;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,60 +25,141 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        insertWordIntoUserDictionary("abc");
+        Button addButton = findViewById(R.id.add_button);
+        View.OnClickListener onAddButtonClicked = v -> onClickAddDetails(v);
+        addButton.setOnClickListener(onAddButtonClicked);
 
-        TextView mSearchWord = findViewById(R.id.search_edit_Text);
-        Button mSearchButton = findViewById(R.id.search_button);
+        Button showButton = findViewById(R.id.show_button);
+        View.OnClickListener onShowButtonClicked = v -> onClickShowDetails(v);
+        showButton.setOnClickListener(onShowButtonClicked);
 
-        View.OnClickListener searchOnClickListener = v -> searchWord(mSearchWord.getText().toString());
-        mSearchButton.setOnClickListener(searchOnClickListener);
+        Button searchButton = findViewById(R.id.search_button);
+        View.OnClickListener onSearchButtonClicked = v -> onClickSearchDetails(v);
+        searchButton.setOnClickListener(onSearchButtonClicked);
+
+        Button updateButton = findViewById(R.id.update_button);
+        View.OnClickListener onUpdateButtonClicked = v -> onClickUpdateDetails(v);
+        updateButton.setOnClickListener(onUpdateButtonClicked);
+
+        Button deleteButton = findViewById(R.id.delete_button);
+        View.OnClickListener onDeleteButtonClicked = v -> onClickDeleteDetails(v);
+        deleteButton.setOnClickListener(onDeleteButtonClicked);
+
+        String message = "Xin chào từ MainActivity";
+        Intent intent = new Intent(this, HelloIntentService.class);
+        intent.putExtra("message", message);
+        startService(intent);
     }
 
-    public void searchWord(String word) {
-        String[] mProjection = {
-                UserDictionary.Words._ID,
-                UserDictionary.Words.WORD,
-                UserDictionary.Words.LOCALE
-        };
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-        String mSelectionClause = UserDictionary.Words.WORD + " like ?";
-        String[] mSelectionArgs = {"%" + word + "%"};
-
-        String mSortOrder = "_id asc";
-
-        Cursor mCursor = getContentResolver().query(
-                UserDictionary.Words.CONTENT_URI,
-                mProjection,
-                mSelectionClause,
-                mSelectionArgs,
-                mSortOrder
-        );
-
-
-        if (mCursor != null) {
-            Integer t = mCursor.getColumnIndex(word);
-            Log.d("ddd", t.toString());
-            int index = mCursor.getColumnIndex(UserDictionary.Words.WORD);
-            while (mCursor.moveToNext()) {
-                String newWord = mCursor.getString(index);
-                Log.d("test", newWord);
-            }
-            mCursor.close();
-        } else {
-            Log.d("test", "Cursor is null");
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 1000010:
+                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                break;
+            case 1000009:
+                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+                break;
         }
+        return true;
     }
 
-    public void insertWordIntoUserDictionary(String word) {
+
+
+    public void onClickAddDetails(View view) {
         ContentValues values = new ContentValues();
-        values.put(UserDictionary.Words.WORD, word);
-        values.put(UserDictionary.Words.LOCALE, "en_US");
-
-        Uri mNewUri;
-        mNewUri = getContentResolver().insert(UserDictionary.Words.CONTENT_URI, values);
-        if (mNewUri == null) {
-            Log.d("asdasd", String.valueOf(getContentResolver().insert(UserDictionary.Words.CONTENT_URI, values)));
+        String tam = ((EditText) findViewById(R.id.first_edit_Text)).getText().toString();
+        if (tam != "") {
+            values.put(MyContentProvider.name, ((EditText) findViewById(R.id.first_edit_Text)).getText().toString());
+            getContentResolver().insert(MyContentProvider.CONTENT_URI, values);
+            Toast.makeText(getBaseContext(), "New Record Inserted", Toast.LENGTH_LONG).show();
         }
+    }
 
+    @SuppressLint("Range")
+    public void onClickSearchDetails(View view) {
+        String searchId = ((EditText) findViewById(R.id.first_edit_Text)).getText().toString();
+        if (!searchId.isEmpty()) {
+            TextView resultView= findViewById(R.id.text_show);
+            String mProjection[] = {
+                    MyContentProvider.id,
+                    MyContentProvider.name,
+            };
+            String mSelection = MyContentProvider.name + " like ?";
+            String mSelectionArgs[] = {searchId};
+            Cursor cursor = getContentResolver().query(Uri.parse("content://com.demo.user.provider/users"), mProjection, mSelection, mSelectionArgs, null);
+            if(cursor.moveToFirst()) {
+                StringBuilder strBuild=new StringBuilder();
+                while (!cursor.isAfterLast()) {
+                    strBuild.append("\n"+cursor.getString(cursor.getColumnIndex("id"))+ "-"+ cursor.getString(cursor.getColumnIndex("name")));
+                    cursor.moveToNext();
+                }
+                resultView.setText(strBuild);
+            }
+            else {
+                resultView.setText("No Records Found");
+            }
+        }
+    }
+
+    @SuppressLint("Range")
+    public void onClickShowDetails(View view) {
+        TextView resultView= findViewById(R.id.text_show);
+        Cursor cursor = getContentResolver().query(Uri.parse("content://com.demo.user.provider/users"), null, null, null, null);
+        if(cursor.moveToFirst()) {
+            StringBuilder strBuild=new StringBuilder();
+            while (!cursor.isAfterLast()) {
+                strBuild.append("\n"+cursor.getString(cursor.getColumnIndex("id"))+ "-"+ cursor.getString(cursor.getColumnIndex("name")));
+                cursor.moveToNext();
+            }
+            resultView.setText(strBuild);
+        }
+        else {
+            resultView.setText("No Records Found");
+        }
+    }
+
+    public void onClickDeleteDetails(View view) {
+        String deleteId = ((EditText) findViewById(R.id.first_edit_Text)).getText().toString();
+        if (!deleteId.isEmpty()) {
+            Uri uriToDelete = Uri.withAppendedPath(MyContentProvider.CONTENT_URI, deleteId);
+            String selection = MyContentProvider.name + "=?";
+            String[] selectionArgs = new String[]{deleteId};
+            int rowsDeleted = getContentResolver().delete(uriToDelete, selection, selectionArgs);
+            if (rowsDeleted > 0) {
+                Toast.makeText(getBaseContext(), "Record Deleted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getBaseContext(), "No Record Deleted", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(getBaseContext(), "Please enter an ID to delete", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onClickUpdateDetails(View view) {
+        String updateId = ((EditText) findViewById(R.id.first_edit_Text)).getText().toString();
+        String newValue = ((EditText) findViewById(R.id.second_edit_Text)).getText().toString();
+        
+        if (!updateId.isEmpty() && !newValue.isEmpty()) {
+            Uri uriToUpdate = Uri.withAppendedPath(MyContentProvider.CONTENT_URI, updateId);
+            ContentValues values = new ContentValues();
+            values.put(MyContentProvider.name, newValue);
+            String where = MyContentProvider.name + "=?";
+            String[] selectionArgs = new String[]{updateId};
+            int rowsUpdated = getContentResolver().update(uriToUpdate, values, where, selectionArgs);
+            if (rowsUpdated > 0) {
+                Toast.makeText(getBaseContext(), "Record Updated", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getBaseContext(), "No Record Updated", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(getBaseContext(), "Please enter an ID and a new value to update", Toast.LENGTH_LONG).show();
+        }
     }
 }
